deb5ab569cb2a09544c4b30903daf1f5c9f40423
Ibrahim.H <bitsnaps@yahoo.fr>
2022-07-26 00:14:24 +0100

    add buefy example

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,22 @@
 <template>
-  <h1>Vite & Vue2</h1>
+  <section class="section">
+    <div class="container">
+      <img class="logo" src="/vite.svg">
+      <h1 class="title">Vite & Vue2 & Buefy</h1>      
+      <b-button type="is-success" @click="clickMe">Click me!</b-button>
+      <p class="subtitle">Find out about <a class="read-the-docs" href="https://buefy.org/documentation">Buefy docs</a>.</p>
+    </div>
+  </section>
 </template>
+
+
+<script>
+    export default {
+        methods: {
+            clickMe() {
+                this.$buefy.notification.open('Clicked!!')
+            }
+        }
+    }
+</script>
 
